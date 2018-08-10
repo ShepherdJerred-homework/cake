8c75800d07df37bde23fbed4dd4924781d21af33
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-08-09 19:44:03 -0500

    Memoize solution

--- src/cake.java
+++ src/cake.java
@@ -1,10 +1,13 @@
 // cake
 // Jerred Shepherd
+// The nested hashmap is a crime, but hey it's fast
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -30,7 +33,7 @@ public class cake {
                     .skip(1)
                     .toArray();
 
-            int max = solve(layers, 0, 0);
+            int max = solve(layers, 0, 0, new HashMap<>());
             System.out.println(max);
             printWriter.println(max);
         }
@@ -46,23 +49,37 @@ public class cake {
         return diameterOfPreviousLayer == 0 || diameterOfLayer <= diameterOfPreviousLayer;
     }
 
-    public static int solve(int[] layers, int currentLayer, int diameterOfPreviousLayer) {
+    public static int solve(int[] layers, int currentLayer, int diameterOfPreviousLayer, Map<Integer, Map<Integer, Integer>> memo) {
         int diameterOfCurrentLayer = layers[currentLayer];
+
+        if (memo.containsKey(currentLayer)) {
+            if (memo.get(currentLayer).containsKey(diameterOfPreviousLayer)) {
+                return memo.get(currentLayer).get(diameterOfPreviousLayer);
+            }
+        }
 
         if (currentLayer == layers.length - 1) {
             return canTakeLayer(diameterOfCurrentLayer, diameterOfPreviousLayer) ? 1 : 0;
         } else {
             int maxHeight;
-            int maxSizeIfLayerIsSkipped = solve(layers, currentLayer + 1, diameterOfPreviousLayer);
+            int maxSizeIfLayerIsSkipped = solve(layers, currentLayer + 1, diameterOfPreviousLayer, memo);
             int maxHeightIfLayerIsTaken;
 
             if (canTakeLayer(diameterOfCurrentLayer, diameterOfPreviousLayer)) {
-                maxHeightIfLayerIsTaken = 1 + solve(layers, currentLayer + 1, diameterOfCurrentLayer);
+                maxHeightIfLayerIsTaken = 1 + solve(layers, currentLayer + 1, diameterOfCurrentLayer, memo);
                 maxHeight = max(maxHeightIfLayerIsTaken, maxSizeIfLayerIsSkipped);
             } else {
                 maxHeight = maxSizeIfLayerIsSkipped;
             }
 
+
+            if (memo.containsKey(currentLayer)) {
+                memo.get(currentLayer).put(diameterOfPreviousLayer, maxHeight);
+            } else {
+                Map<Integer, Integer> newMap = new HashMap<>();
+                newMap.put(diameterOfPreviousLayer, maxHeight);
+                memo.put(currentLayer, newMap);
+            }
             return maxHeight;
         }
     }
